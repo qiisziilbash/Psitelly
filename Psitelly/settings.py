37f285b6ecb0c47b093af8b8a9ad418c4aa32874
qiisziilbash <m.shafiei0423@gmail.com>
2019-11-27 23:03:59 -0700

    debug false

--- Psitelly/settings.py
+++ Psitelly/settings.py
@@ -127,7 +127,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = "/media/"
 
 
-EMAIL_USE_TLS = True
+EMAIL_USE_TLS = False
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'psitelly@gmail.com'
