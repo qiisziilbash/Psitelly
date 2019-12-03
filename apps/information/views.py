7ee1fd8536d31be95ffd10989defa52267624b0e
qiisziilbash <m.shafiei0423@gmail.com>
2019-12-03 11:40:31 -0700

    g reCaptcha is being fixed

--- apps/information/views.py
+++ apps/information/views.py
@@ -1,6 +1,11 @@
+import urllib
+from urllib.request import urlopen
+import json
+
 from django.core.mail import EmailMessage
 from django.shortcuts import render, redirect
 
+from Psitelly import settings
 from apps.information.models import *
 from apps.videos.models import Video, Journal, Author, Topic, Focus
 
@@ -32,20 +37,46 @@ def contact(request):
                    }
         return render(request, 'information/Contact.html', context)
     if request.method == "POST":
-        content = request.POST.get('content', '')
-        email = request.POST.get('email', '')
-        subject = request.POST.get('subject', '')
-        msg = EmailMessage(subject, content, 'psitelly@gmail.com', ['mshafiei0423@gmail.com'], reply_to=[email])
+        ''' Begin reCAPTCHA validation '''
+        recaptcha_response = request.POST.get('g-recaptcha-response')
+        url = 'https://www.google.com/recaptcha/api/siteverify'
+        values = {
+            'secret': settings.GOOGLE_RECAPTCHA_SECRET_KEY,
+            'response': recaptcha_response
+        }
 
-        if 'picture1' in request.FILES:
-            picture1 = request.FILES['picture1']
-            msg.attach(picture1.name, picture1.read(), picture1.content_type)
-        if 'picture2' in request.FILES:
-            picture2 = request.FILES['picture2']
-            msg.attach(picture2.name, picture2.read(), picture2.content_type)
-        if 'picture3' in request.FILES:
-            picture3 = request.FILES['picture3']
-            msg.attach(picture3.name, picture3.read(), picture3.content_type)
+        data = urllib.urlencode(values)
+        response = urlopen(url, data)
+        result = json.load(response)
+        ''' End reCAPTCHA validation '''
+        if result['success']:
+            content = request.POST.get('content', '')
+            email = request.POST.get('email', '')
+            subject = request.POST.get('subject', '')
+            msg = EmailMessage(subject, content, 'psitelly@gmail.com', ['mshafiei0423@gmail.com'], reply_to=[email])
 
-        msg.send()
-        return redirect('contact')
+            if 'picture1' in request.FILES:
+                picture1 = request.FILES['picture1']
+                msg.attach(picture1.name, picture1.read(), picture1.content_type)
+            if 'picture2' in request.FILES:
+                picture2 = request.FILES['picture2']
+                msg.attach(picture2.name, picture2.read(), picture2.content_type)
+            if 'picture3' in request.FILES:
+                picture3 = request.FILES['picture3']
+                msg.attach(picture3.name, picture3.read(), picture3.content_type)
+
+            msg.send()
+            return redirect('contact')
+        else:
+            context = {'newses': News.objects.order_by('-time')[:5],
+                       'Statistics': True,
+                       'usersCount': User.objects.filter().count(),
+                       'videosCount': Video.objects.filter().count(),
+                       'journalCount': Journal.objects.filter().count(),
+                       'authorCount': Author.objects.filter().count(),
+                       'topicCount': Topic.objects.filter().count(),
+                       'focusCount': Focus.objects.filter().count(),
+                       'msgs': ['Invalid reCAPTCHA. Please try again.']
+                       }
+            return render(request, 'information/Contact.html', context)
+
