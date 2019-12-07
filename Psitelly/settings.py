568fa2834db52137b015b508175366a75eb8ee54
qiisziilbash <m.shafiei0423@gmail.com>
2019-12-07 11:51:31 -0700

    radio and check boxes were styled

--- Psitelly/settings.py
+++ Psitelly/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '@gbusud8%-gs(iyid_9)zii&5%n7y09n!fdr*w4na1h&*^5(2x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
+DEBUG = False   
 
 
 ALLOWED_HOSTS = ['*']
