dc20d667c585c193be7daa1631d80b0a34ba7787
qiisziilbash <m.shafiei0423@gmail.com>
2019-12-07 11:59:09 -0700

    mobile view is fixed for profile view and edit

--- Psitelly/settings.py
+++ Psitelly/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '@gbusud8%-gs(iyid_9)zii&5%n7y09n!fdr*w4na1h&*^5(2x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False   
+DEBUG = True
 
 
 ALLOWED_HOSTS = ['*']
