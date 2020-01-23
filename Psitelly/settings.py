b418ddf4577fd15dcdca863f6b729324a8092564
qiisziilbash <m.shafiei0423@gmail.com>
2020-01-23 14:14:16 -0700

    website becomes shareable

--- Psitelly/settings.py
+++ Psitelly/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'apps.comments',
     'apps.information',
     'apps.filter',
-    'taggit'
+    'taggit',
+    'django_social_share',
 ]
 
 MIDDLEWARE = [
@@ -66,6 +67,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.request',
             ],
         },
     },
